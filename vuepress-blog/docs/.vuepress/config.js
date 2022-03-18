0aa8bfa1f4bd7c21ed985afea4e445d9a07d92ad
captainlintong <lintong784533@163.com>
2022-03-18 09:21:41 +0800

    webrtc

--- vuepress-blog/docs/.vuepress/config.js
+++ vuepress-blog/docs/.vuepress/config.js
@@ -58,12 +58,19 @@ module.exports = {
           children: ["/document/js/", "/document/js/Array.md"],
         },
         {
-          title: "pdf预览", // 必要的
-          path: "/document/pdf", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          title: "webrtc", // 必要的
+          path: "/document/webrtc", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
           collapsable: true, // 可选的, 默认值是 true,
           sidebarDepth: 1, // 可选的, 默认值是 1
-          children: ["/document/pdf/"],
+          children: ["/document/webrtc/"],
         },
+        // {
+        //   title: "pdf预览", // 必要的
+        //   path: "/document/pdf", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        //   collapsable: true, // 可选的, 默认值是 true,
+        //   sidebarDepth: 1, // 可选的, 默认值是 1
+        //   children: ["/document/pdf/"],
+        // },
       ],
       "/article/": [
         {
---
+++ vuepress-blog/docs/document/webrtc/README.md
@@ -0,0 +1,2 @@
+# webrtc
+  [mdn链接](https://developer.mozilla.org/zh-CN/docs/Web/API/WebRTC_API)
